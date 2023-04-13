ff5f5f1ba7af2d9a9a56fb181359bf64245caed2
ravi teja <raviteja@echortech.com>
2023-04-13 09:16:43 +0530

    fileadded

--- Manager/Component/Home.js
+++ Manager/Component/Home.js
@@ -1,27 +1,122 @@
-import { View,Text,StyleSheet } from "react-native"
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
-import Group from "../img/Group.svg"
-import Vector from "../img/Vector.svg"
 
-const Home=()=>{
-    
-    return(
+
+const Home = () => {
+  return (
+    <ScrollView style={{backgroundColor:"black"}} > 
+      <View style={styles.back}>
         <View>
-        <Group  />
-        <Vector/>
-        <Text style={styles.text} >Raviteja</Text>
-        <Text style={styles.text1} >Vijay Ram chandran</Text>
+          <Image
+            style={{height: 700, opacity: 0.3}}
+            source={require('../img/imageback.png')}
+          />
         </View>
-    )
-}
-export default Home
-const styles=StyleSheet.create({
-   text:{
-   fontFamily:"DancingScript-VariableFont_wght",
-   fontSize:20
-   },
-   text1:{
-    fontFamily:"DancingScript-VariableFont_wght",
-    fontSize:40,
-   }
-})
+        <View position={'absolute'}>
+          <View style={styles.frontview}>
+            <Image
+              source={require('../img/Vector.png')}
+              style={{width: 30, height: 30, marginRight: '10%'}}
+            />
+            <Text style={{color: '#FFFFFF', fontSize: 25,fontWeight:"900"}}>Welcome,HUBO !</Text>
+          </View>
+          <Text style={styles.time}>12:48</Text>
+          <TouchableOpacity style={styles.btn}>
+            <View style={styles.singpore}>
+              <Text style={{color: '#FFFFFF', marginRight: '10%',fontSize:20}}>
+                Singapore
+              </Text>
+              <Image
+                source={require('../img/Polygon.png')}
+                style={{width: 10, height: 10, marginTop: 7}}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.bottom} >
+        <View  style={styles.group} >
+        <Image
+        source={require('../img/Group1.png')}
+        style={{width:75,height: 78,marginLeft:30}}
+        />
+        <View style={styles.contactbox} >
+        <Text style={{marginLeft:10,fontSize:20,fontWeight:"800",color:"#4F73F5",lineHeight:39}}>connected</Text>
+        <Text style={{color:"#000000",fontWeight:"800"}} >112.684.195.1.3</Text>
+        </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Home;
+const styles = StyleSheet.create({
+  back: {
+    backgroundColor: 'black',
+    height: '60%',
+    
+  },
+  text: {
+    fontFamily: 'DancingScript-VariableFont_wght',
+    color: 'white',
+    fontSize: 20,
+  },
+  bottom:{
+  backgroundColor:"#4F73F5",
+  height:"40%",
+  borderTopLeftRadius:30,
+  borderTopRightRadius:30,
+  width:"100%"
+  
+  },
+  btn: {
+    marginLeft: '30%',
+    marginTop: '10%',
+  },
+  time: {
+    fontSize: 80,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    marginLeft: '30%',
+    marginTop: '20%',
+  },
+  frontview: {
+    flexDirection: 'row',
+    marginLeft: '10%',
+    marginTop: '15%',
+  },
+  singpore: {
+    borderWidth: 1,
+    borderColor:"#FFFFFF",
+    flexDirection: 'row',
+    width: '70%',
+    padding: '8%',
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    borderRadius: 10,
+    justifyContent: 'space-between',
+  },
+  group:{
+   marginLeft:"20%",
+   width:"58%",
+   marginTop:"10%",
+   padding:"10%",
+   borderRadius:168,
+   borderWidth:1,
+   borderColor:"white",
+   backgroundColor:"white",
+   elevation:1,
+  },
+  contactbox:{
+     marginTop:"10%",
+     marginLeft:"20%",
+  }
+});
